67250b55c026daeb1a22333e3e756e0c256d62d7
Eduardo Sandoval <DesarrolloMVC4@hotmail.com>
2025-06-10 17:22:55 -0600

    SCRIPT PARA MARCAS DE VEHICULOS

---
+++ Sql_Scripts/INSERT [dbo].[CatMarcasVehiculos].[DEMO].sql
@@ -0,0 +1,26 @@
+USE [DepositoVehicular_DB]
+GO
+SET IDENTITY_INSERT [dbo].[CatMarcasVehiculos] ON 
+GO
+INSERT [dbo].[CatMarcasVehiculos] ([Id], [Marca], [Activo]) VALUES (1, N'VOLKSWAGEN', 1)
+GO
+INSERT [dbo].[CatMarcasVehiculos] ([Id], [Marca], [Activo]) VALUES (2, N'MITSUBISHI', 1)
+GO
+INSERT [dbo].[CatMarcasVehiculos] ([Id], [Marca], [Activo]) VALUES (3, N'CHEVROLET', 1)
+GO
+INSERT [dbo].[CatMarcasVehiculos] ([Id], [Marca], [Activo]) VALUES (4, N'BMW', 1)
+GO
+INSERT [dbo].[CatMarcasVehiculos] ([Id], [Marca], [Activo]) VALUES (5, N'AUDI', 1)
+GO
+INSERT [dbo].[CatMarcasVehiculos] ([Id], [Marca], [Activo]) VALUES (6, N'TOYOTA', 1)
+GO
+INSERT [dbo].[CatMarcasVehiculos] ([Id], [Marca], [Activo]) VALUES (7, N'NISSAN', 1)
+GO
+INSERT [dbo].[CatMarcasVehiculos] ([Id], [Marca], [Activo]) VALUES (8, N'MERCEDEZ-BENZ', 1)
+GO
+INSERT [dbo].[CatMarcasVehiculos] ([Id], [Marca], [Activo]) VALUES (9, N'MAZDA', 1)
+GO
+INSERT [dbo].[CatMarcasVehiculos] ([Id], [Marca], [Activo]) VALUES (10, N'FORD', 1)
+GO
+SET IDENTITY_INSERT [dbo].[CatMarcasVehiculos] OFF
+GO
